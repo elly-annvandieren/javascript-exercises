f04fdf8692ff5fc7f68009050c454c9c5b2d25b9
elly-annvandieren <elly-annvandieren@hotmail.nl>
2025-04-17 19:11:36 +0200

    03_reverseString v1 (needs optimization)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let inputReversed = []
+    const inputArray = input.split("");
+    while (inputArray.length !== 0) {
+        inputReversed.unshift(inputArray[0]);
+        inputArray.shift();
+    }
+    return inputReversed.join("");
 };
+
+// npm test reverseString.spec.js
+
+// can be simplified:
+//  const reverseString = function (string) {
+//     return string.split("").reverse().join("");
+//  };
 
 // Do not edit below this line
 module.exports = reverseString;
