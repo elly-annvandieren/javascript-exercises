ceb4744a3f7d36e4f1502fe44ba9472a7cad2c74
elly-annvandieren <elly-annvandieren@hotmail.nl>
2025-04-17 19:45:35 +0200

    finished 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    for (let item of items) {
+        while (array.includes(item)) {
+            let itemIndex = array.indexOf(item)
+            array.splice(itemIndex, 1);
+        }
+    }
+    return array;
 };
+
+// npm test removeFromArray.spec.js
 
 // Do not edit below this line
 module.exports = removeFromArray;
