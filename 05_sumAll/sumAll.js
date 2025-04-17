e80e97ac9a8c7b6ba17b807de0f3e941011b8335
elly-annvandieren <elly-annvandieren@hotmail.nl>
2025-04-17 20:07:26 +0200

    finished 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (
+        a < 0 || 
+        b < 0 || 
+        typeof a !== 'number' || 
+        typeof b !== 'number' || 
+        !Number.isInteger(a) || 
+        !Number.isInteger(b)
+    ) {
+        return "ERROR";
+    }
 
+    if (a > b) {
+        [a, b] = [b, a];
+    }
+
+    let total = 0;
+    for (let i = a; i <= b; i++) {
+        total += i;
+    }
+
+    return total;
 };
+// npm test sumAll.spec.js
 
 // Do not edit below this line
 module.exports = sumAll;
